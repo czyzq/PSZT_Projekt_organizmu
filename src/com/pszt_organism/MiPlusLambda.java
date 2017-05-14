250ddf7ec1b8eaf2c0be5c6618d6c7b526615ae8
konrad <konradszk@gmail.com>
2017-05-14 17:47:05 +0200

    Dodane 3 puste klasy - teraz poprawnie

---
+++ src/com/pszt_organism/MiPlusLambda.java
@@ -0,0 +1,7 @@
+package com.pszt_organism;
+
+/**
+ * Created by konrad on 14.05.2017.
+ */
+public class MiPlusLambda {
+}
---
+++ src/com/pszt_organism/Organisms.java
@@ -0,0 +1,15 @@
+package com.pszt_organism;
+
+import java.awt.*;
+import java.util.HashSet;
+
+/**
+ * Created by konrad on 14.05.2017.
+ */
+public class Organisms {
+
+    private HashSet<Point> mPoints;
+    private int cosdopusha;
+
+    public Organisms(){};
+}
