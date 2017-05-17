673f37c1f1fbe6d367d85dd70e94846dbfb40e6b
KarolinaCabaj <karolina.anna.cabaj@gmail.com>
2017-05-17 21:32:05 +0200

    mutacja z losowaniem dla kazdego genu czy mutacja czy nie

--- src/com/pszt_organism/MiPlusLambda.java
+++ src/com/pszt_organism/MiPlusLambda.java
@@ -10,14 +10,35 @@ public class MiPlusLambda {
     public static int[]mutacja(int[] Wektor, int min, int max)
     {
         //tak wlasciwie min to zawsze 0 a max o bedzie 2*potegaDwojki*m-1
-        int[]TabZmutowana = Wektor;
-        int randomNum = ThreadLocalRandom.current().nextInt(min, max);
-        System.out.print("\n numer random: "+randomNum);
-        if(TabZmutowana[randomNum]==1)
-            TabZmutowana[randomNum]=0;
-        else
-            TabZmutowana[randomNum]=1;
 
-        return TabZmutowana;
+        //tablica prawdopodobienstwa takiej dlugosci jak wektor, bo prawdopodobienstwo zanegowania bitu ma by 1/dl_wektora
+        int[] prawdopodobienstwo = new int[Wektor.length];
+        for (int i = 0; i < prawdopodobienstwo.length; i++)
+        {
+            prawdopodobienstwo[i] = 0;
+        }
+        //gdzies bedzie 1 i jezeli ja sie wylosuje to znaczy ze gen ma byc negowany
+        int randomZmiana = ThreadLocalRandom.current().nextInt(0, max);
+        prawdopodobienstwo[randomZmiana]=1;
+
+        int[]WektorZmutowany = Wektor;
+        for(int w=0; w<WektorZmutowany.length; w++)
+        {
+            int randomNum = ThreadLocalRandom.current().nextInt(min, max);
+            //System.out.print("\n numer random: "+randomNum);
+            //jezeli losowy index z prawdopodobienstwa jest 1 to gen o indeksie w mutuje
+            if(prawdopodobienstwo[randomNum ]==1)
+            {
+                //jezli byl 1 to mutuje na 0
+                if(WektorZmutowany[w]==1)
+                    WektorZmutowany[w]=0;
+                    //jezeli byl 0 to mutuje na 1
+                else
+                    WektorZmutowany[w]=1;
+            }
+        }
+
+
+        return WektorZmutowany;
     }
 }
